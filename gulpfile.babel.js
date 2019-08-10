e4b3c2c8c706d0a5d76f3745bce0e696a4a8fce4
장봉기 <bkjang@jangbong-giui-MacBookPro.local>
2019-08-11 00:13:00 +0900

    📦image build task 작성

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
 import ws from 'gulp-webserver';
+import gimage from 'gulp-image';
 
 const routes = {
   pug : {
     watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
+  },
+  img : {
+    src: "src/img/*",
+    dest: "build/img"
   }
 };
 
@@ -22,11 +27,17 @@ const webserver = () => gulp
   .src("build")
   .pipe(ws({ livereload: true, open: true }));
 
+const img = () => gulp
+  .src(routes.img.src)
+  .pipe(gimage())
+  .pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug); // 2번째 인자는 어떤 작업을 수행할 것인지
+  gulp.watch(routes.img.src, img);
 };
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
